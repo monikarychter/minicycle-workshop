723e70678974159eb34b31ff826e65bbed00fe66
Cool Lin <andrzejkom2016@gmail.com>
2020-01-11 15:20:10 +0100

    ready to test

--- adventure-python/adventure.py
+++ adventure-python/adventure.py
@@ -1,4 +1,8 @@
 
+outside = {'e': 'cave', 'n': 'heaven', 's': 'hole', 'w': 'sea'}
+cave = {'w':'outside', 'e':'east_corridor', 's': 'south_corridor'}
+heaven = {'s': "outside", 'n': 'hell'}
+str_dict = {'l_outside': outside, "l_cave": cave, "l_heaven": heaven}
 
 player = {
     'room': 'outside',
@@ -7,7 +11,7 @@ player = {
 rooms = {
     "outside": {
         "title": "Outside",
-        "description": "You are standing outside of a huge cave entrance on the East. Up North there is a ladder",
+        "description": "You are standing outside of a huge cave entrance on the East. Up North is a ladder. South - hole, and West - sea",
     },
     "cave": {
         "title": "Cave",
@@ -16,11 +20,31 @@ rooms = {
     "heaven":{
         "title": "Heaven",
         "description": "You see GOD. Wow"
+    },
+    "hole":{
+        "title": "Hole",
+        "description": "You see a huge hole"
+    },
+    "sea":{
+        "title": "Sea",
+        "description": "You see a boat"
+    },
+    "east_corridor":{
+        "title": "East corridor",
+        "description": "Height around 90 cm and wet wall"
+    },
+    "south_corridor":{
+        "title": "South corridor",
+        "description": "You see a bog"
+    },
+    "hell":{
+        "title": "Hell",
+        "description": "You see ass Piotra"
     }
 }
 
-
 def main():
+    current_location = "l_outside"
     describe_room()
     playing = True
     while playing:
@@ -31,11 +55,21 @@ def main():
             print('Bye!')
             playing = False
         elif command in ['East', 'e']:
-           player['room']= 'cave'
+           player['room']= str_dict[current_location]['e']
            describe_room()
+           current_location = str_dict[current_location]['e']
         elif command in ["North", 'n']:
-            player['room']= 'heaven'
+            player['room']= str_dict[current_location]['n']
             describe_room()
+            current_location = str_dict[current_location]['n']
+        elif command in ["South", 's']:
+            player['room']= str_dict[current_location]['s']
+            describe_room()
+            current_location = str_dict[current_location]['s']
+        elif command in ["West", 'w']:
+            player['room']= str_dict[current_location]['w']
+            describe_room()
+            current_location = str_dict[current_location]['w']    
         else:
             print(f'Unrecognized command: {command}')
 
